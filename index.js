ce98bed83a73148ca27e6e2caf23ec27328a2a2f
Georgiana-Mădălina Dragomir <dragomirgeorgianamadalina@gmail.com>
2024-07-06 00:06:28 +0300

    Fixed the errors

--- index.js
+++ index.js
@@ -1,30 +1,58 @@
 const contacts = require("./contacts");
-const argv = require("yargs").argv;
+const { Command } = require("commander");
+const program = new Command();
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "contact id")
+  .option("-n, --name <type>", "contact name")
+  .option("-e, --email <type>", "contact email")
+  .option("-p, --phone <type>", "contact phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
 
 async function invokeAction({ action, id, name, email, phone }) {
-  switch (action) {
-    case "list":
-      const allContacts = await contacts.listContacts();
-      console.log(allContacts);
-      break;
+  try {
+    switch (action) {
+      case "list":
+        const allContacts = await contacts.listContacts();
+        console.log(allContacts);
+        break;
 
-    case "get":
-      const contact = await contacts.getContactById(id);
-      console.log(contact);
-      break;
+      case "get":
+        const contact = await contacts.getContactById(id);
+        if (contact) {
+          console.log(contact);
+        } else {
+          console.log("Contact not found");
+        }
+        break;
 
-    case "add":
-      const newContact = await contacts.addContact(name, email, phone);
-      console.log(newContact);
-      break;
+      case "add":
+        if (!name || !email || !phone) {
+          console.warn("Missing required fields for adding a contact");
+          break;
+        }
+        const newContact = await contacts.addContact(name, email, phone);
+        console.log(newContact);
+        break;
 
-    case "remove":
-      const updatedContacts = await contacts.removeContact(id);
-      console.log(updatedContacts);
-      break;
+      case "remove":
+        if (!id) {
+          console.warn("Missing ID for removing a contact");
+          break;
+        }
+        const updatedContacts = await contacts.removeContact(id);
+        console.log(updatedContacts);
+        break;
 
-    default:
-      console.warn("\x1B[31m Unknown action type!");
+      default:
+        console.warn("\x1B[31m Unknown action type!");
+    }
+  } catch (error) {
+    console.error(error.message);
   }
 }
 
